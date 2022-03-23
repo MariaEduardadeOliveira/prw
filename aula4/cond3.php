b58eb96dacc53397c58ff8399e589b661150be6d
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-03-23 11:03:39 -0300

    comando switch

---
+++ aula4/cond3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Aula 4 - 23/03/2022</h1>
+	<p><strong>Condições 2 - if/else</strong></p>
+	<?php
+   $op = "a";
+   switch($op){
+   case "a": echo "Abrir"; break;
+   case "f": echo "Fechar"; break;
+   case "s": echo "Salvar"; break;
+   default: echo "Sair"; break;
+   }   
+	?>
+</body>
+</html>
