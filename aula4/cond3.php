5556d347608c37c6524fd13866e2527b963505e7
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-03-23 11:13:38 -0300

    comando switch case

--- aula4/cond3.php
+++ aula4/cond3.php
@@ -5,14 +5,24 @@
 </head>
 <body>
 	<h1>Aula 4 - 23/03/2022</h1>
-	<p><strong>Condições 2 - if/else</strong></p>
+	<p><strong>Condições 3 - switch case</strong></p>
 	<?php
-   $op = "a";
-   switch($op){
-   case "a": echo "Abrir"; break;
-   case "f": echo "Fechar"; break;
-   case "s": echo "Salvar"; break;
-   default: echo "Sair"; break;
+    $op = "a"; //Opção A
+    
+    //Abaixo Lista de Opções
+    echo "Lista de Opções: <br>
+       a - Abrir <br>
+       f - Fechar <br>
+       s - Salvar <br>
+       default - Sair <br> <br>";
+    
+    echo "Opção Escolhida: ", $op, "<br>";
+
+    switch($op){ //Caso Opção "...", Resultado "..."
+    case "a": echo "Resultado: Abrir"; break;
+    case "f": echo "Resultado: Fechar"; break;
+    case "s": echo "Resultado: Salvar"; break;
+    default: echo "Resultado: Sair"; break;
    }   
 	?>
 </body>
