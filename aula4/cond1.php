aa1a96b364e0b6ba2deacd2ebff86d49e9eec4ba
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-03-23 10:45:14 -0300

    Exemplo If

---
+++ aula4/cond1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Aula 4 - 23/03/2022</h1>
+	<p><strong>Condições 1 - if e if/else</strong></p>
+	<?php
+    $n1 = 7; //Primeira Nota
+    $n2 = 8; //Segunda Nota
+    $media = ($n1 + $n2)/2; //Média das Notas
+    
+    echo "Nota 1: ", $n1, "<br>";
+    echo "Nota 2: ", $n2, "<br>";
+    echo "Média: ", $media, "<br>";
+    
+    if($media >= 7) //Aprovado se média maior ou igual à 7
+       echo "Você foi aprovado";
+	?>
+</body>
+</html>
