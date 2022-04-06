30cce073be275cc5db06ebeabcdab28ecc9fb035
Maria Eduarda de Oliveira <101722802+MariaEduardadeOliveira@users.noreply.github.com>
2022-04-06 11:45:21 -0300

    Add files via upload

---
+++ lista1/exercicio2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Lista 1 - 16/03/2022</h1>
+	<p><strong>Exercício 2</strong></p>
+	<?php
+	$r = 2; //Raio do Círculo
+	$area = (3.14 * ($r*$r)); //A Área do Círculo
+
+	echo "O Raio desse Círculo é de: ", $r, "<br>";
+	echo "A Área desse Círculo é de: ", $area;
+	?>
+</body>
+</html>
