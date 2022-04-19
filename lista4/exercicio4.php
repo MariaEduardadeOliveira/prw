f904e0da47fe0506e3da8bfdabd04eb9695b1975
Maria Eduarda de Oliveira <101722802+MariaEduardadeOliveira@users.noreply.github.com>
2022-04-19 20:46:48 -0300

    Create exercicio4.php

---
+++ lista4/exercicio4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <h1>Lista 4 - 30/03/2022</h1>
+    <p><strong>Exercício 4</strong></p>
+    <?php
+    $ano = $_POST['ano'];
+
+    if($ano%4 == 0 && $ano%100 != 0){
+        echo "O ano ", $ano, " É bissexto.";
+    }else{
+        echo "O ano ", $ano, " NÃO é bissexto.";
+    }
+	?>
+</body>
+</html>
