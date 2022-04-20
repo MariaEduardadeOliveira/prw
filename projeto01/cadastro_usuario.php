6c6f3a3dae90ad4863373cc83fa34744916ab07d
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-04-20 09:24:50 -0300

    mysql

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+    $nome_usuario = $_POST['nome_usuario'];
+    $email_usuario = $_POST['email_usuario'];
+    $fone_usuario = $_POST['fone_usuario'];
+
+    echo "<p>Nome do usuário: ", $nome_usuario, "<br>";
+    echo "E-mail: ", $email_usuario, "<br>";
+    echo "Telefone: ", $fone_usuario, "</p>";
+	?>
+</body>
+</html>
