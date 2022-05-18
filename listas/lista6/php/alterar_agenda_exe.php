e673e9a24b1b922a401cfff07964c8195e7a54b1
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-05-18 10:55:33 -0300

    a

--- listas/lista6/php/alterar_agenda_exe.php
+++ listas/lista6/php/alterar_agenda_exe.php
@@ -6,6 +6,51 @@
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
 <body>
-    <a class="a" href="../php/index.php">Voltar</a>
+    <?php
+    include('conexao.php');
+    $id_agenda = $_POST['id_agenda'];
+    $nome= $_POST['nome'];
+    $apelido= $_POST['apelido'];
+    $endereco= $_POST['endereco'];
+    $bairro= $_POST['bairro'];
+    $cidade= $_POST['cidade'];
+    $estado= $_POST['estado'];
+    $telefone= $_POST['telefone'];
+    $celular= $_POST['celular'];
+    $email= $_POST['email'];
+    $dt_cadastro= date('Y-m-d');
+
+    $sql = "update agenda set 
+    nome='" . $nome . "',
+    apelido='" . $apelido . "', 
+    endereco='" . $endereco . "', 
+    bairro='" . $bairro . "',
+    cidade='" . $cidade . "',
+    estado='" . $estado . "',
+    telefone='" . $telefone . "',
+    celular='" . $celular . "',
+    email='" . $email . "', 
+    dt_cadastro='". $dt_cadastro . "' 
+    where id_agenda=" . $id_agenda;
+
+    $result = mysqli_query($con, $sql);
+
+    if($result){
+        echo "<p>Nome: ", $nome, "<br>";
+        echo "Apelido: ", $apelido, "<br>";
+        echo "Endereço: ", $endereco, "<br>";
+        echo "Bairro: ", $bairro, "<br>";
+        echo "Cidade: ", $cidade, "<br>";
+        echo "Estado: ", $estado, "<br>";
+        echo "Telefone: ", $telefone, "<br>";
+        echo "Celular: ", $celular, "<br>";
+        echo "E-mail: ", $email, "<br>";
+        echo "Data de Cadastro:", date('d/m/Y'), "<br></p>";
+        echo "Dados alterados com sucesso.<br>";
+    }else{
+        echo "Erro ao alterar no banco de dados.<br>", mysqli_error($con);
+    }
+	?>
+    <a class="a" href="index.php">Voltar</a>
 </body>
 </html>
