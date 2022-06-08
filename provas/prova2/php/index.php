d71c7c3910e06450878a819cb69f9b48dfde2467
Maria Eduarda de Oliveira <maria.oliveira1@aluno.ifsp.edu.br>
2022-06-08 09:22:34 -0300

    test

---
+++ provas/prova2/php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title></title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <h2 class="h2">Menu Principal</h2>
+    <ul id="menu">
+        <li><a class="a" href="../html/cadastro_agenda.html">Cadastrar Agenda</a></li>
+        <li><a class="a" href="listar_agenda.php">Listar Agenda</a></li>
+    </ul>
+</body>
+</html>
